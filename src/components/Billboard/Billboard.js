c81f1ac5a59b66d674627a5806fde9ed29ce9ef4
Kesbeo <wezeng@ucsd.edu>
2022-10-17 04:15:01 -0700

    feat - implemented basic billboard without video

--- src/components/Billboard/Billboard.js
+++ src/components/Billboard/Billboard.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+import BillboardInfo from "./BillboardInfo";
+
+import cover from "./assets/billboard-cover.jpg";
+
 import styles from "./Billboard.module.css";
 
 const Billboard = () => {
-  return <div className={styles.billboard}>Billboard</div>;
+  return (
+    <div className={styles.billboard}>
+      <div className={styles["billboard-video-container"]}>Video Container</div>
+      <div className={styles["billboard-img-container"]}>
+        <img
+          className={styles["billboard-img"]}
+          src={cover}
+          alt="billboard-cover-img"
+        />
+        <div class={styles["billboard-img-vignette"]} />
+      </div>
+      <BillboardInfo />
+    </div>
+  );
 };
 
 export default Billboard;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <React.Fragment>
+      {props.href ? (
+        <a href={props.href}>
+          <button className={props.className} type="button">
+            {props.children}
+          </button>
+        </a>
+      ) : (
+        <button className={props.className} type="button">
+          {props.children}
+        </button>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Button;
